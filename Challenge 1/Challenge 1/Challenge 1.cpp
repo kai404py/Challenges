c9a993add433922776de7741fda41e6ea487dbf2
Kai <Kaiboucher2007@gmail.com>
2025-11-23 06:54:20 +0000

    Add initial C++ solutions for Challenges 1-3
    Added Visual Studio solution and project files for three C++ challenges. Challenge 1 prints an ASCII boat, Challenge 2 calculates experience needed for next level, and Challenge 3 outputs 'Hello World!'. Includes all necessary source files, project configurations, and filters.

---
+++ Challenge 1/Challenge 1/Challenge 1.cpp
@@ -0,0 +1,25 @@
+﻿//
+//  main.cpp
+//  Boat
+//
+//  Created by Kai on 14/11/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+    std::cout << "\n";
+    std::cout << "                  |\n";
+    std::cout << "                 /|\\\n";
+    std::cout << "                / | \\ a boat\n";
+    std::cout << "               /  |  \\\n";
+    std::cout << "              / --|-- \\\n";
+    std::cout << "             /    |    \\\n";
+    std::cout << "            /-----|-----\\\n";
+    std::cout << "     |----\\       |      /----|\n";
+    std::cout << "      \\- - -------------- - -/\n";
+    std::cout << "       \\ - -- - - - - - -- -/\n";
+    std::cout << " /\\  /\\ \\/\\  /\\  /\\  /\\  /\\/ /\\  /\\\n";
+    std::cout << "/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\\n";
+    return 0;
+}
